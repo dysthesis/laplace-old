215a85c26257d05abc15e1aae23bd43793ec159a
Dysthesis <antheoraviel@protonmail.com>
2024-02-15 23:56:33 +0000

    Mark /nix/persist as neededForBoot
    Impermanence needs to be able to access the persistence filesystem at boot to build the system. Therefore, set fileSystems."/nix/persist".neededForBoot to true.

--- hosts/phobos/default.nix
+++ hosts/phobos/default.nix
@@ -1,10 +1,13 @@
 {inputs, ...}: {
   imports = [
-		# This is our hardware-specific configurations
     ./hardware-configuration.nix
 
     # We use disko to partition and manage our disks and filesystems
     inputs.disko.nixosModules.default
     (import ./disko.nix {device = "/dev/nvme0n1";})
   ];
+
+  # Prioritise the /nix/persist filesystem for boot
+  # to let impermanence do its thing.
+  fileSystems."/nix/persist".neededForBoot = true;
 }
