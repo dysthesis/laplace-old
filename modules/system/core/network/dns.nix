f5fc4f8eb0a7183c20ab544fb81e2cca771ff971
Dysthesis <antheoraviel@protonmail.com>
2024-02-16 16:05:43 +1100

    Create a module for encrypted DNS resolvers.
    This utilises dnscrypt-proxy. It should be possible to extend this option to manually select the DNS resolvers, but I don't need them yet for now.

---
+++ modules/system/core/network/dns.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options.my.network.encrypted-dns.enable = lib.mkEnableOption "Whether or not to enable encrypted DNS.";
+  config = lib.mkIf config.my.network.encrypted-dns.enable {
+    services = {
+      dnscrypt-proxy2 = {
+        enable = true;
+        settings = {
+          ipv6_servers = true;
+          require_dnssec = true;
+
+          sources.public-resolvers = {
+            urls = [
+              "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+              "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+            ];
+            cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+            minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+          };
+        };
+      };
+    };
+  };
+}
