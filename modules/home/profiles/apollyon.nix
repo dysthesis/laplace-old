6d62334f315c482eaa0cba7d3a0a3ccdd35c0543
Dysthesis <antheoraviel@protonmail.com>
2024-02-17 09:49:44 +1100

    Move home impermanence to where it should be.

--- modules/home/profiles/apollyon.nix
+++ modules/home/profiles/apollyon.nix
@@ -1,9 +1,27 @@
-{
+{lib, ...}:
+with lib; {
   # Let home-manager manage itself
   programs.home-manager.enable = true;
   home = {
     username = "apollyon";
     homeDirectory = "/home/apollyon";
     home.stateVersion = "23.11";
+  };
+
+  home.persistence."/nix/persist/home/apollyon" = {
+    allowOther = true;
+    directories =
+      [
+        "Org"
+        "Zotero"
+        "Downloads"
+        "Documents"
+        "Music"
+        "Pictures"
+        ".ssh"
+        ".gnupg"
+      ]
+      # .config directories to persist
+      ++ forEach ["emacs" "doom"] (x: ".config/${x}");
   };
 }
--- modules/system/core/impermanence/default.nix
+++ modules/system/core/impermanence/default.nix
@@ -4,47 +4,24 @@
   lib,
   ...
 }: {
-  options = {
-    my.impermanence = {
-      enable = lib.mkEnableOption "Enable system impermanence";
-      user.enable = lib.mkEnableOption "Enable user impermanence";
+  options.my.impermanence.enable = lib.mkEnableOption "Enable system impermanence";
+
+  config = lib.mkIf config.my.impermanence.enable {
+    imports = [inputs.impermanence.nixosModule];
+    programs.fuse.userAllowOther = true;
+    environment.persistence."/nix/persist" = {
+      hideMounts = true;
+      directories = [
+        "/etc/nixos"
+        "/etc/NetworkManager"
+        "/etc/ssh"
+        "/etc/NetworkManager/system-connections"
+        "/var/lib/flatpak"
+        "/var/lib/libvirt"
+        "/var/lib/bluetooth"
+        "/etc/secureboot"
+      ];
+      files = ["/etc/machine-id"];
     };
   };
-
-  config =
-    lib.mkIf config.my.impermanence.enable {
-      imports = [inputs.impermanence.nixosModule];
-      environment.persistence."/nix/persist" = {
-        hideMounts = true;
-        directories = [
-          "/etc/nixos"
-          "/etc/NetworkManager"
-          "/etc/ssh"
-          "/etc/NetworkManager/system-connections"
-          "/var/lib/flatpak"
-          "/var/lib/libvirt"
-          "/var/lib/bluetooth"
-          "/etc/secureboot"
-        ];
-        files = ["/etc/machine-id"];
-      };
-    }
-    // lib.mkIf config.my.impermanence.user.enable {
-      programs.fuse.userAllowOther = true;
-      home.persistence."/nix/persist/home" = {
-        directories = [
-          "Documents"
-          "Downloads"
-          "Music"
-          "Pictures"
-          "Org"
-          ".gnupg"
-          ".nixops"
-          ".local/share/keyrings"
-          ".local/share/direnv"
-          ".config/emacs"
-          ".config/doom"
-        ];
-      };
-    };
 }
