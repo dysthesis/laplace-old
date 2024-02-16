9f2dc57d260c1f70e579f606ac4d94b3eb271c74
Dysthesis <antheoraviel@protonmail.com>
2024-02-16 18:33:18 +1100

    Add the secure boot module.

---
+++ modules/system/core/security/secure-boot/default.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.my.security.secure-boot.enable = lib.mkEnableOption "Whether or not to enable secure boot.";
+  config = lib.mkIf config.my.security.secure-boot.enable {
+    # Can't use systemd-boot with Lanzaboote
+    my.boot.systemd-boot.enable = lib.mkForce false;
+    environment.systemPackages = [
+      pkgs.sbctl
+    ];
+    # Lanzaboote currently replaces the systemd-boot module.
+    # This setting is usually set to true in configuration.nix
+    # generated at installation time. So we force it to false
+    # for now.
+    boot.loader.systemd-boot.enable = lib.mkForce false;
+
+    boot = {
+      bootspec.enable = true;
+      lanzaboote = {
+        enable = true;
+        pkiBundle = "/etc/secureboot";
+      };
+    };
+  };
+}
