3a71f06645f475308177a3cf8cb27c87188085fe
Dysthesis <antheoraviel@protonmail.com>
2024-02-16 13:12:47 +1100

    Yoink lib from vimjoyer/nixconf

---
+++ lib/default.nix
@@ -0,0 +1,34 @@
+# This is mostly stolen from https://github.com/vimjoyer/nixconf/blob/main/myLib/default.nix
+{inputs}: let
+  myLib = (import ./default.nix) {inherit inputs;};
+  inherit (inputs.self) outputs;
+in rec {
+  # ======================= Package Helpers ======================== #
+
+  pkgsFor = sys: inputs.nixpkgs.legacyPackages.${sys};
+
+  # ========================== Buildables ========================== #
+
+  mkSystem = config:
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs outputs myLib;
+      };
+      modules = [
+        config
+        outputs.nixosModules.default
+      ];
+    };
+
+  mkHome = sys: config:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = pkgsFor sys;
+      extraSpecialArgs = {
+        inherit inputs myLib outputs;
+      };
+      modules = [
+        config
+        outputs.homeManagerModules.default
+      ];
+    };
+}
