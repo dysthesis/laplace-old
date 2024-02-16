efcc0f1edec8c0c28a18d2137b9b3edd2784f594
Dysthesis <antheoraviel@protonmail.com>
2024-02-17 09:31:25 +1100

    Enable home-manager for phobos

--- hosts/phobos/default.nix
+++ hosts/phobos/default.nix
@@ -5,8 +5,17 @@
 
     # We use disko to partition and manage our disks and filesystems
     inputs.disko.nixosModules.default
+
+    # We want to use home-manager to manage our user configs
+    inputs.home-manager.nixosModules.home-manager
     (import ./disko.nix {device = "/dev/nvme0n1";})
   ];
+
+  homeManager = {
+    useUserPackages = true;
+    useGlobalPackages = true;
+    extraSpecialArgs = {inherit inputs;};
+  };
 
   # Prioritise the /nix/persist and /nix/persist/home filesystem for boot
   # to let impermanence do its thing.
