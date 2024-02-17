fb5be5e5ec6673dfd89322c532b9dde044da89c6
Dysthesis <antheoraviel@protonmail.com>
2024-02-18 10:00:13 +1100

    Add custom packages as nixpkgs overlay
    Eliminate redundant code, since most of these packages will be called on multiple locations anyways.

--- modules/system/core/nix/overlays.nix
+++ modules/system/core/nix/overlays.nix
@@ -1,5 +1,11 @@
 {inputs, ...}: {
   nixpkgs.overlays = [
     inputs.emacs-overlay.overlay
+    (self: super: {
+      sf-pro = super.callPackage ../../../../pkgs/sf-pro {};
+      georgia-pro = super.callPackage ../../../../pkgs/georgia-fonts {};
+      cartograph-nf = super.callPackage ../../../../pkgs/cartograph-nf {};
+      sugar-dark-sddm = super.callPackage ../../../../pkgs/sugar-dark-sddm {};
+    })
   ];
 }
