50e8dc043b81ccb810cef1c0d9559eea5059cf9e
Dysthesis <antheoraviel@protonmail.com>
2024-02-18 10:22:37 +1100

    Create polkit user service

--- modules/home/users/apollyon/services/default.nix
+++ modules/home/users/apollyon/services/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./gnupg
+    ./polkit
   ];
 }
---
+++ modules/home/users/apollyon/services/polkit/default.nix
@@ -0,0 +1,15 @@
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  mkGraphicalService = lib.recursiveUpdate {
+    Unit.PartOf = ["graphical-session.target"];
+    Unit.After = ["graphical-session.target"];
+    Install.WantedBy = ["graphical-session.target"];
+  };
+in {
+  systemd.user.services.polkit-pantheon-authentication-agent-1 = mkGraphicalService {
+    Service.ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+  };
+}
