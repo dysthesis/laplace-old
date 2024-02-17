e38926f8e6ec4a6b37a2264324b6876d35b7be11
Dysthesis <antheoraviel@protonmail.com>
2024-02-18 10:40:07 +1100

    Add fish configuration

---
+++ modules/home/users/apollyon/shell/starship/default.nix
@@ -0,0 +1,25 @@
+{
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      add_newline = true;
+      scan_timeout = 5;
+      character = {
+        error_symbol = "[󰊠](bold red)";
+        success_symbol = "[󰊠](bold green)";
+        vicmd_symbol = "[󰊠](bold yellow)";
+        format = "$symbol [|](bold bright-black) ";
+      };
+      git_commit = {commit_hash_length = 4;};
+      line_break.disabled = false;
+      lua.symbol = "[](blue) ";
+      python.symbol = "[](blue) ";
+      hostname = {
+        ssh_only = true;
+        format = "[$hostname](bold blue) ";
+        disabled = false;
+      };
+    };
+  };
+}
