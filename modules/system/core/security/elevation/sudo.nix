33724dc7658f2a8c4c82864dccae722c3c320c0d
Dysthesis <antheoraviel@protonmail.com>
2024-02-18 10:06:21 +1100

    Disable sudo lectures.
    Impermanence causes sudo to always display the lecture after startup, so it's less annoying to just disable letures outright.

--- modules/system/core/security/elevation/sudo.nix
+++ modules/system/core/security/elevation/sudo.nix
@@ -8,6 +8,12 @@
       wheelNeedsPassword = true;
       execWheelOnly = true;
       enable = true;
+
+      # Impermanence means that sudo will forget that it already displayed the lecture
+      # so disable it outright for convenience's sake.
+      extraConfig = ''
+        Defaults lecture = never
+      '';
     };
     security.doas.enable = false;
   };
