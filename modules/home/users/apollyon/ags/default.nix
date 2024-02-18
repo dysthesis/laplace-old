8e7576bff22df9412362cda699bc6e0e15521ff3
Dysthesis <antheoraviel@protonmail.com>
2024-02-18 17:39:23 +1100

    Yoink end-4's AGS configs

---
+++ modules/home/users/apollyon/ags/default.nix
@@ -0,0 +1,38 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.ags.homeManagerModules.default
+  ];
+
+  home.packages = with pkgs; [
+    #ollama
+    pywal
+    sassc
+    (python311.withPackages (p: [
+      p.material-color-utilities
+      p.pywayland
+    ]))
+  ];
+
+  programs.ags = {
+    enable = true;
+    configDir = ./config; # if ags dir is managed by home-manager, it'll end up being read-only. not too cool.
+    # configDir = ./.config/ags;
+
+    extraPackages = with pkgs; [
+      gtksourceview
+      gtksourceview4
+      # ollama
+      python311Packages.material-color-utilities
+      python311Packages.pywayland
+      pywal
+      sassc
+      webkitgtk
+      webp-pixbuf-loader
+      ydotool
+    ];
+  };
+}
