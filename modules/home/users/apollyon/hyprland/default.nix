5f020d28af42f6e3354cb58054573ed862f1cd44
Dysthesis <antheoraviel@protonmail.com>
2024-02-18 16:55:58 +1100

    Hyprland window group appearance configuration

--- modules/home/users/apollyon/hyprland/default.nix
+++ modules/home/users/apollyon/hyprland/default.nix
@@ -149,6 +149,17 @@ in {
         no_gaps_when_only = false;
       };
 
+      group = {
+        "col.border_active" = "rgba(89b4faff)";
+        "col.border_inactive" = "rgba(6c7086ff)";
+        groupbar = {
+          font_family = "JetBrainsMono NF";
+          font_size = 8;
+          "col.active" = "rgba(89b4faff)";
+          "col.inactive" = "rgba(6c7086ff)";
+        };
+      };
+
       ## ANIMATIONS ##
       animations = {
         enabled = true;
