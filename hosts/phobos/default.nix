ad42ff7bd171547825c70be8389db5d65f420c0f
Dysthesis <antheoraviel@protonmail.com>
2024-02-19 10:32:25 +1100

    Forgot to disable the home persistence dataset

--- hosts/phobos/default.nix
+++ hosts/phobos/default.nix
@@ -20,5 +20,5 @@
   # Prioritise the /nix/persist and /nix/persist/home filesystem for boot
   # to let impermanence do its thing.
   fileSystems."/nix/persist".neededForBoot = true;
-  fileSystems."/nix/persist/home".neededForBoot = true;
+  #fileSystems."/nix/persist/home".neededForBoot = true;
 }
