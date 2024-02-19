2a878da45a557242cb92e71bdc2b58fde3e33c51
Dysthesis <antheoraviel@protonmail.com>
2024-02-19 12:29:41 +1100

    Reduce wezterm cell width to make fonts look less weird

--- modules/home/users/apollyon/wezterm/default.nix
+++ modules/home/users/apollyon/wezterm/default.nix
@@ -24,7 +24,7 @@
         },
 
         font_size = 10,
-        cell_width = 0.9,
+        cell_width = 0.8,
        -- Font config
         font = font_with_fallback(font_name),
         font_rules = {
