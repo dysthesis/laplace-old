e9b97a7fa4efc39499d895ce67f19392c4955a05
Dysthesis <antheoraviel@protonmail.com>
2024-02-20 16:14:29 +1100

    Install git-remote-gcrypt

--- modules/home/users/apollyon/git/default.nix
+++ modules/home/users/apollyon/git/default.nix
@@ -1,5 +1,7 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [zsh-forgit gitflow];
+  home.packages = with pkgs; [
+    git-remote-gcrypt
+  ];
   programs.git = {
     enable = true;
     userName = "Dysthesis";
