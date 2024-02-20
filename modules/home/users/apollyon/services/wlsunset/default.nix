e5c8cb2276fafb739130cfea9baf483223242df4
Dysthesis <antheoraviel@protonmail.com>
2024-02-20 16:15:28 +1100

    Create wlsunset user service

---
+++ modules/home/users/apollyon/services/wlsunset/default.nix
@@ -0,0 +1,12 @@
+{
+  wlsunset = {
+    enable = true;
+    systemd.target = "hyprland-session.target";
+    latitude = "-33.8917232";
+    longitude = "151.2020636";
+    temperature = {
+      day = 6200;
+      night = 3700;
+    };
+  };
+}
