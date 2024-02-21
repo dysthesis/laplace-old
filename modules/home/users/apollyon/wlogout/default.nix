0bc67f66b979b43a61452b4dd558e8ace5e97213
Dysthesis <antheoraviel@protonmail.com>
2024-02-21 11:48:01 +1100

    wlogout works basically flawlessly now

--- modules/home/users/apollyon/wlogout/default.nix
+++ modules/home/users/apollyon/wlogout/default.nix
@@ -35,19 +35,19 @@
     ];
     style = ''
       window {
-          font-family: JetBrainsMono Nerd FOnt;
-          font-size: 14pt;
-          color: #cdd6f4; /* text */
-          background-color: rgba(30, 30, 46, 0.8);
+          font-family: SF Pro DIsplay;
+          font-size: 12pt;
+          color: #ffffff; /* text */
+          background-color: rgba(0, 0, 0, 0.8);
       }
 
       button {
           background-repeat: no-repeat;
           background-position: center;
-          background-size: 50%;
+          background-size: 25%;
           border: none;
-          background-color: rgba(30, 30, 46, 0);
-          margin: 5px;
+          background-color: rgba(255, 255, 255, 0);
+          margin: 8px;
           transition: box-shadow 0.2s ease-in-out, background-color 0.2s ease-in-out;
       }
 
@@ -56,56 +56,44 @@
       }
 
       button:focus {
-          background-color: #b4befe;
-          color: #1e1e2e;
+          background-color: #ffffff;
+          color: #000000;
       }
 
       #lock {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/lock.png"));
+          background-image: image(url("${./lock.png}"));
       }
       #lock:focus {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/lock-hover.png"));
+          background-image: image(url("${./lock-hover.png}"));
       }
 
       #logout {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/logout.png"));
+          background-image: image(url("${./logout.png}"));
       }
       #logout:focus {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/logout-hover.png"));
+          background-image: image(url("${./logout-hover.png}"));
       }
 
       #suspend {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/sleep.png"));
+          background-image: image(url("${./sleep.png}"));
       }
       #suspend:focus {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/sleep-hover.png"));
+          background-image: image(url("${./sleep-hover.png}"));
       }
 
       #shutdown {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/power.png"));
+          background-image: image(url("${./power-hover.png}"));
       }
       #shutdown:focus {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/power-hover.png"));
+          background-image: image(url("${./power-hover.png}"));
       }
 
       #reboot {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/restart.png"));
+          background-image: image(url("${./restart.png}"));
       }
       #reboot:focus {
-          background-image: image(url("${config.home.homeDirectory}/.config/wlogout/icons/restart-hover.png"));
+          background-image: image(url("${./restart-hover.png}"));
       }
     '';
-  };
-  home.file = {
-    ".config/wlogout/icons/lock.png".source = ./lock.png;
-    ".config/wlogout/icons/lock-hover.png".source = ./lock-hover.png;
-    ".config/wlogout/icons/logout.png".source = ./logout.png;
-    ".config/wlogout/icons/logout-hover.png".source = ./logout-hover.png;
-    ".config/wlogout/icons/sleep.png".source = ./sleep.png;
-    ".config/wlogout/icons/sleep-hover.png".source = ./sleep-hover.png;
-    ".config/wlogout/icons/power.png".source = ./power.png;
-    ".config/wlogout/icons/power-hover.png".source = ./power-hover.png;
-    ".config/wlogout/icons/restart.png".source = ./restart.png;
-    ".config/wlogout/icons/restart-hover.png".source = ./restart-hover.png;
   };
 }
