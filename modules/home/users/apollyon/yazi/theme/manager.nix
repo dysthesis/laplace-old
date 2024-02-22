270c12f4fc1a32a004eb101ad05ff2139304311e
Dysthesis <antheoraviel@protonmail.com>
2024-02-22 17:32:06 +1100

    Yazi theming?

--- modules/home/users/apollyon/yazi/theme/manager.nix
+++ modules/home/users/apollyon/yazi/theme/manager.nix
@@ -1,63 +1,213 @@
 {
-  programs.yazi.theme.manager = {
-    cwd = { fg = "cyan"; };
+  programs.yazi.theme = {
+    manager = {
+      cwd = {fg = "#94e2d5";};
 
-    # Hovered
-    hovered = {
-      fg = "black";
-      bg = "lightblue";
+      # Hovered
+      hovered = {
+        fg = "#000000";
+        bg = "#89b4fa";
+      };
+
+      preview_hovered = {
+        underline = true;
+      };
+
+      # Find
+      find_keyword = {
+        fg = "#f9e2af";
+        italic = true;
+      };
+
+      find_position = {
+        fg = "#f5c2e7";
+        bg = "reset";
+        italic = true;
+      };
+
+      # Marker
+      marker_selected = {
+        fg = "#89b4fa";
+        bg = "#89b4fa";
+      };
+      marker_copied = {
+        fg = "#a6e3a1";
+        bg = "#a6e3a1";
+      };
+      marker_cut = {
+        fg = "#f38ba8";
+        bg = "#f38ba8";
+      };
+
+      # Tab
+      tab_active = {
+        fg = "#000000";
+        bg = "#ffffff";
+      };
+      tab_inactive = {
+        fg = "white";
+        bg = "#45475a";
+      };
+      tab_width = 1;
+
+      # Border;
+      border_symbol = "│";
+      border_style = {fg = "#7f849c";};
+
+      # Offset;
+      folder_offset = [1 0 1 0];
+      preview_offset = [1 1 1 1];
+
+      # Highlighting;
+      syntect_theme = "${./Catppuccin-mocha.tmTheme}";
+    };
+    status = {
+      separator_open = "";
+      separator_close = "";
+      separator_style = {
+        fg = "#45475a";
+        bg = "#45475a";
+      };
+
+      # Mode
+      mode_normal = {
+        fg = "#1e1e2e";
+        bg = "#89b4fa";
+        bold = true;
+      };
+      mode_select = {
+        fg = "#1e1e2e";
+        bg = "#a6e3a1";
+        bold = true;
+      };
+      mode_unset = {
+        fg = "#1e1e2e";
+        bg = "#f2cdcd";
+        bold = true;
+      };
+
+      # Progress
+      progress_label = {
+        fg = "#ffffff";
+        bold = true;
+      };
+      progress_normal = {
+        fg = "#89b4fa";
+        bg = "#45475a";
+      };
+      progress_error = {
+        fg = "#f38ba8";
+        bg = "#45475a";
+      };
+
+      # Permissions
+      permissions_t = {fg = "#89b4fa";};
+      permissions_r = {fg = "#f9e2af";};
+      permissions_w = {fg = "#f38ba8";};
+      permissions_x = {fg = "#a6e3a1";};
+      permissions_s = {fg = "#7f849c";};
     };
 
-    preview_hovered = {
-      fg = "black";
-      bg = "lightblue";
+    input = {
+      border = {fg = "#89b4fa";};
+      title = {};
+      value = {};
+      selected = {reversed = true;};
     };
 
-    # Find
-    find_keyword = {
-      fg = "yellow";
-      italic = true;
-    };
-    find_position = {
-      fg = "magenta";
-      bg = "reset";
-      italic = true;
+    select = {
+      border = {fg = "#89b4fa";};
+      active = {fg = "#f5c2e7";};
+      inactive = {};
     };
 
-    # Marker
-    marker_selected = {
-      fg = "lightgreen";
-      # bg = "lightgreen";
-    };
-    marker_copied = {
-      fg = "lightyellow";
-      # bg = "lightyellow";
-    };
-    marker_cut = {
-      fg = "lightred";
-      # bg = "lightred";
+    tasks = {
+      border = {fg = "#89b4fa";};
+      title = {};
+      hovered = {underline = true;};
     };
 
-    # Tab
-    tab_active = {
-      fg = "black";
-      bg = "lightblue";
+    which = {
+      mask = {bg = "#313244";};
+      cand = {fg = "#94e2d5";};
+      rest = {fg = "#9399b2";};
+      desc = {fg = "#f5c2e7";};
+      separator = "  ";
+      separator_style = {fg = "#585b70";};
     };
-    tab_inactive = {
-      fg = "white";
-      bg = "darkgray";
+
+    help = {
+      on = {fg = "#f5c2e7";};
+      exec = {fg = "#94e2d5";};
+      desc = {fg = "#9399b2";};
+      hovered = {
+        bg = "#585b70";
+        bold = true;
+      };
+      footer = {
+        fg = "#45475a";
+        bg = "#cdd6f4";
+      };
     };
-    tab_width = 1;
 
-    # Border;
-    border_symbol = "│";
-    border_style = { fg = "gray"; };
+    filetype = {
+      rules = [
+        # Images
+        {
+          mime = "image/*";
+          fg = "#94e2d5";
+        }
 
-    # Offset;
-    folder_offset = [ 1 0 1 0 ];
-    preview_offset = [ 1 1 1 1 ];
+        # Videos
+        {
+          mime = "video/*";
+          fg = "#f9e2af";
+        }
+        {
+          mime = "audio/*";
+          fg = "#f9e2af";
+        }
 
-    # Highlighting;
-    syntect_theme = "";
+        # Archives
+        {
+          mime = "application/zip";
+          fg = "#f5c2e7";
+        }
+        {
+          mime = "application/gzip";
+          fg = "#f5c2e7";
+        }
+        {
+          mime = "application/x-tar";
+          fg = "#f5c2e7";
+        }
+        {
+          mime = "application/x-bzip";
+          fg = "#f5c2e7";
+        }
+        {
+          mime = "application/x-bzip2";
+          fg = "#f5c2e7";
+        }
+        {
+          mime = "application/x-7z-compressed";
+          fg = "#f5c2e7";
+        }
+        {
+          mime = "application/x-rar";
+          fg = "#f5c2e7";
+        }
+
+        # Fallback
+        {
+          name = "*";
+          fg = "#cdd6f4";
+        }
+        {
+          name = "*/";
+          fg = "#89b4fa";
+        }
+      ];
+    };
   };
 }
