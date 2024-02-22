8eeb237cf753d1e5c042e0a9791763c97d377c17
Dysthesis <antheoraviel@protonmail.com>
2024-02-22 15:57:33 +1100

    Fix btop catppuccin theme sha256

--- modules/home/users/apollyon/btop/default.nix
+++ modules/home/users/apollyon/btop/default.nix
@@ -10,7 +10,7 @@
   xdg.configFile = {
     "btop/themes/catppuccin_mocha.theme".source = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/catppuccin/btop/c6469190f2ecf25f017d6120bf4e050e6b1d17af/themes/catppuccin_mocha.theme";
-      hash = "sha256-KnXUnp2sAolP7XOpNhX2g8m26josrqfTycPIBifS90Y=";
+      hash = "sha256-TeaxAadm04h4c55aXYUdzHtFc7pb12e0wQmCjSymuug=";
     };
   };
 }
