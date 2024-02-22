ebfd4e5a339413121b1b18bb4d41a4b37d4d99c2
Dysthesis <antheoraviel@protonmail.com>
2024-02-22 16:56:12 +1100

    Fix sha256sum for the catppuccin bat theme

--- modules/home/users/apollyon/bat/default.nix
+++ modules/home/users/apollyon/bat/default.nix
@@ -14,6 +14,6 @@
   };
   xdg.configFile."bat/themes/Catppuccin-mocha.tmTheme".source = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/catppuccin/bat/ba4d16880d63e656acced2b7d4e034e4a93f74b1/Catppuccin-mocha.tmTheme";
-    sha256 = "0vys09k1jj8hv4ra4qvnrhwxhn48c2gxbxmagb3dyg7kywh49wvg";
+    sha256 = "sha256:1z434yxjq95bbfs9lrhcy2y234k34hhj5frwmgmni6j8cqj0vi58";
   };
 }
