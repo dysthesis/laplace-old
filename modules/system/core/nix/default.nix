6c631e156dc8705916f6453841777f1eb589980d
Dysthesis <antheoraviel@protonmail.com>
2024-02-22 19:38:54 +1100

    Let nix install obsidian

--- modules/system/core/nix/default.nix
+++ modules/system/core/nix/default.nix
@@ -1,15 +1,30 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./garbage-cleaning.nix
     ./nvd.nix
     ./overlays.nix
   ];
 
-  config.nix = {
-    package = pkgs.nixFlakes;
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-      auto-optimise-store = true;
+  config = {
+    nix = {
+      package = pkgs.nixFlakes;
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
+        auto-optimise-store = true;
+      };
+    };
+    nixpkgs.config = {
+      allowUnfreePredicate = pkg:
+        builtins.elem (lib.getName pkg) [
+          "obsidian"
+        ];
+      permittedInsecurePackages = [
+        "electron-25.9.0"
+      ];
     };
   };
 }
