8e1eab4ab021c52b290840143a5f820982b5444b
Dysthesis <antheoraviel@protonmail.com>
2024-02-22 17:35:38 +1100

    Zathura theming

--- modules/home/users/apollyon/zathura/default.nix
+++ modules/home/users/apollyon/zathura/default.nix
@@ -6,36 +6,36 @@
       default-bg = "#000000";
 
       completion-bg = "#313244";
-      completion-fg = "#CDD6F4";
+      completion-fg = "#FFFFFF";
       completion-highlight-bg = "#575268";
-      completion-highlight-fg = "#CDD6F4";
+      completion-highlight-fg = "#FFFFFF";
       completion-group-bg = "#313244";
       completion-group-fg = "#89B4FA";
 
-      statusbar-fg = "#CDD6F4";
+      statusbar-fg = "#FFFFFF";
       statusbar-bg = "#313244";
 
       notification-bg = "#313244";
-      notification-fg = "#CDD6F4";
+      notification-fg = "#FFFFFF";
       notification-error-bg = "#313244";
       notification-error-fg = "#F38BA8";
       notification-warning-bg = "#313244";
       notification-warning-fg = "#FAE3B0";
 
-      inputbar-fg = "#CDD6F4";
+      inputbar-fg = "#FFFFFF";
       inputbar-bg = "#313244";
 
       recolor = true;
-      recolor-lightcolor = "#1E1E2E";
-      recolor-darkcolor = "#CDD6F4";
+      recolor-lightcolor = "#000000";
+      recolor-darkcolor = "#FFFFFF";
 
-      index-fg = "#CDD6F4";
-      index-bg = "#1E1E2E";
-      index-active-fg = "#CDD6F4";
+      index-fg = "#FFFFFF";
+      index-bg = "#000000";
+      index-active-fg = "#FFFFFF";
       index-active-bg = "#313244";
 
-      render-loading-bg = "#1E1E2E";
-      render-loading-fg = "#CDD6F4";
+      render-loading-bg = "#000000";
+      render-loading-fg = "#FFFFFF";
 
       highlight-color = "#575268";
       highlight-fg = "#F5C2E7";
