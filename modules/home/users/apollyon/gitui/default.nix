4c92a495137403cfe7378af0f45bea0a6c59e5c0
Dysthesis <antheoraviel@protonmail.com>
2024-02-24 09:41:27 +1100

    Check off some tasks.

--- modules/home/users/apollyon/gitui/default.nix
+++ modules/home/users/apollyon/gitui/default.nix
@@ -1,9 +1,9 @@
-{
+{pkgs, ...}: {
   programs.gitui = {
     enable = true;
-    theme = builtins.fetchurl {
+    theme = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/catppuccin/gitui/main/theme/mocha.ron";
-      hash = "sha256-TeaxAadm04h4c55aXYUdzHtFc7pb12e0wQmCjSymuug=";
+      hash = "sha256-i0WUnSoi9yL+OEgn5b2w7f9bqVYzBkt9zNaysSJAYLY=";
     };
   };
 }
