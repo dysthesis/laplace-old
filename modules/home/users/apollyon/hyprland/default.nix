dbff8d87b3929cde6321fcc1209c3e0ddce036a5
Dysthesis <antheoraviel@protonmail.com>
2024-02-25 10:48:56 +1100

    Update windowrules for hyprland

--- modules/home/users/apollyon/hyprland/default.nix
+++ modules/home/users/apollyon/hyprland/default.nix
@@ -100,10 +100,12 @@ in {
 
       windowrulev2 = [
         "workspace 1, class:^(firefox)$"
-        "workspace 3, class:^(FreeTube)$"
         "workspace 2, class:^(vesktop)$"
         "workspace 2, class:^(Element)$"
+        "workspace 3, class:^(FreeTube)$"
+        "workspace 4, class:^(virt-manager)$"
         "workspace 5, class:^(thunderbird)$"
+        "workspace 6, class:^(steam)$"
         "float,title:^(Page Info —.*)$"
         "float,class:thunderbird,title:(Enter credentials for)(.*)"
         "float,class:udiskie"
