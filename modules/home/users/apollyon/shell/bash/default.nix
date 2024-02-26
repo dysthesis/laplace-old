8f68004bb39295a16619b87182cbab13141019d5
Dysthesis <antheoraviel@protonmail.com>
2024-02-27 07:38:26 +1100

    Bash autostarts fish

--- modules/home/users/apollyon/shell/bash/default.nix
+++ modules/home/users/apollyon/shell/bash/default.nix
@@ -6,6 +6,11 @@
       if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
        Hyprland
       fi
+      if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+         then
+           shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+           exec fish ''${LOGIN_OPTION}
+      fi
     '';
   };
 }
